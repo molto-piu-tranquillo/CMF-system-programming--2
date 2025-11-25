f21b29f8cc4b652a0b38b5c097cff0ea41d3c32f
Minsu Kim <m.kim@knu.ac.kr>
2025-11-25 16:05:01 +0000

    completed feature/auth

--- auth.h
+++ auth.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 
 #define AUTH_SALT "ufms-demo-salt"
+// 해시 값 생성 -> 터미널에 아래 라인 입력
+// echo -n 'ufms-demo-salt<문자열>' | shasum -a 256
 
 typedef struct {
     char username[64];
@@ -22,6 +24,5 @@ typedef enum {
 void hash_password(const char *password, char out_hex[65]);
 AuthResult verify_credentials(const char *user, const char *provided_hash, int *out_permission_level, int *out_remaining_attempts);
 bool get_user_info(const char *user, UserAccount *out);
-bool auth_init(void);
 
 #endif
